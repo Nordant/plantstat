c7f0fb0e5e8807a863c6ec075b4e1b33516baac0
Maksym Shkliarevskyi <52889393+Nordant@users.noreply.github.com>
2021-07-07 16:44:04 +0300

    v0.3.0

--- plantstat/__init__.py
+++ plantstat/__init__.py
@@ -4,3 +4,4 @@ __author__ = 'Maksym Shkliarevskyi (maksym.shkliarevskyi@gmail.com)'
 
 from .stat_func import Variable_Analyzer
 from .ML_func import AutoML_Classifier, AutoML_Regressor, Clusterer
+from .vision import OpenStomataPredictor, set_seed, dataloader
