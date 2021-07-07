d5ee4f8ff77aad56adf482acb7842212eb1903d1
Maksym Shkliarevskyi <52889393+Nordant@users.noreply.github.com>
2021-07-07 18:03:40 +0300

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+import plantstat
 
 setup(name = 'PlantStat',
-      version = '0.3.0',
+      version = plantstat.__version__,
       description = 'Package for main statistics and AutoML in plant sciences',
       long_description = 'A package with a set of functions for fast and convenient statistical processing of experimental data. Also includes simple AutoML algorithms and CV of microscopic images. Designed for needs of the LPBPS (Laboratory of Physiology and Biochemistry of Plant Stress, Kharkiv, Ukraine).',
-      url = 'https://github.com/Nordant/plantstat.git',
+      url = 'https://github.com/Nordant/plantstat',
       author = 'Maksym Shkliarevskyi',
       author_email = 'maksym.shkliarevskyi@gmail.com',
       license = 'Apache License 2.0',
-      packages = ['plantstat'],
       keywords = ['python', 'statistics', 'automl'],
       zip_safe = False,
-      include_package_data = True)
+      install_requires=['pandas', 'numpy', 'matplotlib', 'scipy', 'scikit-learn', 'seaborn',
+                        'statsmodels', 'torch', 'torchvision', 'googledrivedownloader', 'opencv'],
+      python_requires='>=3',      
+      packages = find_packages())
